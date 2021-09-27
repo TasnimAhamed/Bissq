7c62224e689affbe2a78333d9c824d323bc0380c
TasnimAhmed <tasnim.ahamed.20@gmail.com>
2021-09-28 04:47:37 +0600

    bissq restaruant project done

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+//Go to top
+
+function goTop() {
+  document.documentElement.scrollTop = 0;
+}
+
+//Mobile Menu
+
+const burgerIcon=document.getElementById("burger-icon")
+const sideNav=document.getElementById("mySidenav")
+burgerIcon.addEventListener('click',()=>{
+  sideNav.classList.toggle("mobileNavStyle");
+})
+burgerIcon.addEventListener('click',()=>{
+  burgerIcon.classList.toggle("open");
+})
+
+//Sticky Header
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+      var navFixed = $(window).scrollTop();
+      if(navFixed > 200){
+          $(".main-nav").addClass("fixed-top fadeInDown");
+          $(".header-banner").addClass("banner-padding");
+      }else{
+          $(".main-nav").removeClass("fixed-top fadeInDown");
+          $(".header-banner").removeClass("banner-padding");
+
+      }
+  });
+})
